b0a7ee1377006d3029ee5196bac025a3154185eb
Kylian <kylian.cadet@epitech.eu>
2022-01-23 15:57:14 +0100

    feat(sql): add more example with conditional orderBy 	modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -29,16 +29,103 @@ app.get('/filtered', async function (req, res) {
 app.get('/ordered', async function (req, res) {
   const queryBuilder = req.dbConnection.getRepository(A).createQueryBuilder('a')
   queryBuilder.leftJoinAndSelect('a.bs', 'b');
+  queryBuilder.addOrderBy(
+    `CASE
+      WHEN "b"."key" = '1' THEN 0
+      ELSE NULL
+    END
+  `, 'ASC');
+
+  const r = await queryBuilder.getMany();
+  return res.status(200).json(r);
+})
+
+app.get('/paginated-ordered-workaround', async function (req, res) {
+  const page = req.query['page'] ? Number(req.query['page']) : 1
+  const item = req.query['item'] ? Number(req.query['item']) : 5
+
+  const queryBuilder = req.dbConnection.getRepository(A).createQueryBuilder('a')
+  queryBuilder.leftJoinAndSelect('a.bs', 'b');
+
+  /**
+   * Sub-queries to order the elements
+   * @returns Arrays of ids ordered
+   */
+  const getOrderedIds = async (): Promise<number[]> => {
+    const queryBuilder = req.dbConnection.getRepository(A).createQueryBuilder('a')
+    queryBuilder.leftJoinAndSelect('a.bs', 'b');
+    queryBuilder.addOrderBy(
+      `CASE
+        WHEN "b"."key" = '1' THEN 0
+        ELSE NULL
+      END
+    `, 'ASC');
+    const r = await queryBuilder.getMany();
+    return r.map((v) => v.id);
+  }
+
+  const orderedIds = await getOrderedIds();
+  queryBuilder.addSelect(
+    `CASE
+      ${orderedIds.map((id, index) => `WHEN "a"."id" = ${id} THEN ${index}`).join('\n')}
+      ELSE NULL
+    END`, 'orderby_key');
+  queryBuilder.addOrderBy('orderby_key');
+
+
+  queryBuilder.skip((page - 1) * item);
+  queryBuilder.take(item);
+
+  console.log(queryBuilder.getSql());
+  const r = await queryBuilder.getMany();
+  return res.status(200).json(r);
+})
+
+app.get('/paginated-ordered-test', async function (req, res) {
+  const page = req.query['page'] ? Number(req.query['page']) : 1
+  const item = req.query['item'] ? Number(req.query['item']) : 5
+
+  const queryBuilder = req.dbConnection.getRepository(A).createQueryBuilder('a')
+  queryBuilder.leftJoinAndSelect('a.bs', 'b');
+
   queryBuilder.addSelect(
     `CASE
       WHEN "b"."key" = '1' THEN 0
       ELSE NULL
-    END`,
-    'order_key'
-  );
-  queryBuilder.orderBy('order_key');
+    END
+  `, 'ordered_key');
+
+  queryBuilder.orderBy('ordered_key');
+
+  queryBuilder.skip((page - 1) * item);
+  queryBuilder.take(item);
 
   const r = await queryBuilder.getMany();
+  return res.status(200).json(r);
+})
+
+app.get('/paginated-ordered', async function (req, res) {
+  const page = req.query['page'] ? Number(req.query['page']) : 1
+  const item = req.query['item'] ? Number(req.query['item']) : 5
+
+  const queryBuilder = req.dbConnection.getRepository(A).createQueryBuilder('a')
+  queryBuilder.leftJoinAndSelect('a.bs', 'b');
+  queryBuilder.addOrderBy(
+    `CASE
+      WHEN "b"."key" = '1' THEN 0
+      ELSE NULL
+    END
+  `, 'ASC');
+
+  queryBuilder.skip((page - 1) * item);
+  queryBuilder.take(item);
+
+  /**
+   * Impossible de getMany en utilisant orderBy + skip / take
+   * Possible d'utiliser getMany avec offset / limit
+   */
+  // const r = await queryBuilder.getMany();
+  const r = await queryBuilder.getRawMany();
   return res.status(200).json(r);
 })
 
@@ -68,7 +155,11 @@ app.get('/paginated-skip-take', async function (req, res) {
   return res.status(200).json(r);
 })
 
-
 app.listen(process.env.PORT, () => {
   console.log(`server listening on port ${process.env.PORT}`)
 })
+
+
+// conditional OrderBy + skip / take ne marche pas
+// conditional OrderBy + offset / limit marche
+// conditional addSelect + orderBy + skip / take -> undefined
